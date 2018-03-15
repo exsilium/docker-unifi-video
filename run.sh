ed41ab815f0df2a2a1d92b403dd6f48f6767fdc0
Exile <exile@chamber.ee>
2018-03-15 09:47:34 +0000

    Unifi-video 3.9.4 release

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ BASEDIR=~/Applications/unifi-video
 IP=0.0.0.0
 
 NAME=unifi-video
-VERSION=v3.9.3
+VERSION=v3.9.4
 
 # Run docker once to create a container and return the ID
 # For following startups, use 'docker start <containerID>'
